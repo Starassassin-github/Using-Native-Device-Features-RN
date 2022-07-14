73103cb97ab5f47ab6531d15774ed4e3e2fbfa78
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-07-15 01:30:05 +0700

    10-LocationUsers-AddInteractiveMap-ConfigAPI
    10-LocationUsers-AddInteractiveMap-ConfigAPI
    Delete Static Map API Project Later

---
+++ util/location.js
@@ -0,0 +1,9 @@
+import config from "../config";
+
+const GOOGLE_API_KEY = config.GOOGLE_API_KEY;
+
+
+export function getMapPreview(lat, lng) {
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
+  return imagePreviewUrl;
+}
